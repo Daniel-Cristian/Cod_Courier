bedc6b613e1edfbbe626af93837736f58dc0ac6a
Daniel <oancea.daniel98@gmail.com>
2023-05-03 16:23:41 +0300

    Update surse de date

--- Courier FDBO/Surse de date/10_Transporturi.sql
+++ Courier FDBO/Surse de date/10_Transporturi.sql
@@ -31,6 +31,6 @@ Insert into TRANSPORTURI values (10,2,1,7,to_date('27-JAN-18','DD-MON-RR'),'12:0
 Insert into TRANSPORTURI values (11,1,11,12,to_date('29-JAN-18','DD-MON-RR'),'15:00',to_date('29-JAN-18','DD-MON-RR'),'19:00','Da',580,220);
 Insert into TRANSPORTURI values (12,1,1,11,to_date('01-FEB-18','DD-MON-RR'),'12:00',to_date('01-FEB-18','DD-MON-RR'),'16:00','Nu',280,42);
 Insert into TRANSPORTURI values (13,1,2,1,to_date('03-FEB-18','DD-MON-RR'),'15:00',to_date('03-FEB-18','DD-MON-RR'),'19:00','Nu',680,210);
-Insert into TRANSPORTURI values (14,2,2,2,to_date('05-FEB-18','DD-MON-RR'),'12:00',to_date('05-FEB-18','DD-MON-RR'),'16:45','Nu',320,89);
-Insert into TRANSPORTURI values (15,3,11,2,to_date('20-FEB-18','DD-MON-RR'),'15:00',to_date('21-FEB-18','DD-MON-RR'),'18:30','Da',650,152);
+Insert into TRANSPORTURI values (10,2,1,7,to_date('03-FEB-18','DD-MON-RR'),'12:00',to_date('03-FEB-18','DD-MON-RR'),'21:00','Nu',650,421);
+Insert into TRANSPORTURI values (11,1,11,12,to_date('10-FEB-18','DD-MON-RR'),'15:00',to_date('10-FEB-18','DD-MON-RR'),'19:00','Da',580,220);
 COMMIT;
