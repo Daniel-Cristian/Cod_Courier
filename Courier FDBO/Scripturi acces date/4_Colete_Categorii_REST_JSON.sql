da867a707568298c8920f888ab815df221477e7e
Daniel-Cristian <90038552+Daniel-Cristian@users.noreply.github.com>
2023-05-05 22:56:53 +0300

    Add files via upload

---
+++ Courier FDBO/Scripturi acces date/4_Colete_Categorii_REST_JSON.sql
@@ -0,0 +1,306 @@
+[{
+  "categorii_colete": {
+    "categorie": [
+      {
+        "ID_CATEGORIE": 11,
+        "DENUMIRE_CATEGORIE": "Plicuri normale",
+        "GREUTATE_MIN": 0,
+        "GREUTATE_MAX": 2,
+        "VALOARE_MIN": 0,
+        "VALOARE_MAX": 200000,
+        "colete": {
+          "colet": [
+            {
+              "id_colet": 1,
+              "greutate": 1,
+              "valoare": 3500,
+              "continut_colet": "Bratara aur 24k"
+            },
+            {
+              "id_colet": 2,
+              "greutate": 1,
+              "valoare": 0,
+              "continut_colet": "Contract Telefonie"
+            },
+            {
+              "id_colet": 3,
+              "greutate": 1,
+              "valoare": 2500,
+              "continut_colet": "Bratara aur alb"
+            },
+            {
+              "id_colet": 4,
+              "greutate": 1,
+              "valoare": 1500,
+              "continut_colet": "Cercei smarald"
+            },
+            {
+              "id_colet": 5,
+              "greutate": 1,
+              "valoare": 2800,
+              "continut_colet": "Colier diamant-aur"
+            },
+            {
+              "id_colet": 6,
+              "greutate": 1,
+              "valoare": 2320,
+              "continut_colet": "Colier diamant-aur"
+            },
+            {
+              "id_colet": 7,
+              "greutate": 1,
+              "valoare": 0,
+              "continut_colet": "Contract televiziune"
+            },
+            {
+              "id_colet": 8,
+              "greutate": 1,
+              "valoare": 250,
+              "continut_colet": "Cercei aur"
+            },
+            {
+              "id_colet": 9,
+              "greutate": 1,
+              "valoare": 2430,
+              "continut_colet": "Inel aur-alb 24k"
+            },
+            {
+              "id_colet": 10,
+              "greutate": 1,
+              "valoare": 5620,
+              "continut_colet": "Colier diamant-aur"
+            },
+            {
+              "id_colet": 11,
+              "greutate": 1,
+              "valoare": 4986,
+              "continut_colet": "Cercei diamant-aur"
+            },
+            {
+              "id_colet": 12,
+              "greutate": 1,
+              "valoare": 0,
+              "continut_colet": "Contract televizune"
+            }
+          ]
+        }
+      },
+      {
+        "ID_CATEGORIE": 15,
+        "DENUMIRE_CATEGORIE": "Electronice mari",
+        "GREUTATE_MIN": 10,
+        "GREUTATE_MAX": 400,
+        "VALOARE_MIN": 0,
+        "VALOARE_MAX": 500000,
+        "colete": {
+          "colet": [
+            {
+              "id_colet": 13,
+              "greutate": 50,
+              "valoare": 4729,
+              "continut_colet": "Combina side Tesla"
+            },
+            {
+              "id_colet": 14,
+              "greutate": 70,
+              "valoare": 1250,
+              "continut_colet": "Masina Samsung"
+            },
+            {
+              "id_colet": 15,
+              "greutate": 75,
+              "valoare": 1350,
+              "continut_colet": "Masina Samsung"
+            },
+            {
+              "id_colet": 16,
+              "greutate": 80,
+              "valoare": 1580,
+              "continut_colet": "Combina Silver"
+            },
+            {
+              "id_colet": 17,
+              "greutate": 65,
+              "valoare": 1210,
+              "continut_colet": "Masina Arctic"
+            },
+            {
+              "id_colet": 18,
+              "greutate": 59,
+              "valoare": 1110,
+              "continut_colet": "Compresor 2500W"
+            },
+            {
+              "id_colet": 19,
+              "greutate": 25,
+              "valoare": 870,
+              "continut_colet": "Kit Bormasina"
+            },
+            {
+              "id_colet": 20,
+              "greutate": 67,
+              "valoare": 1510,
+              "continut_colet": "Compresor 2500W"
+            },
+            {
+              "id_colet": 21,
+              "greutate": 75,
+              "valoare": 2110,
+              "continut_colet": "Compresor 5500W"
+            },
+            {
+              "id_colet": 22,
+              "greutate": 75,
+              "valoare": 1110,
+              "continut_colet": "Aragaz Arctic"
+            },
+            {
+              "id_colet": 23,
+              "greutate": 75,
+              "valoare": 2110,
+              "continut_colet": "Aragaz Arctic-Plita"
+            },
+            {
+              "id_colet": 24,
+              "greutate": 40,
+              "valoare": 1590,
+              "continut_colet": "Masina de spalat vase"
+            }
+          ]
+        }
+      },
+      {
+        "ID_CATEGORIE": 61,
+        "DENUMIRE_CATEGORIE": "Televizoare",
+        "GREUTATE_MIN": 10,
+        "GREUTATE_MAX": 400,
+        "VALOARE_MIN": 0,
+        "VALOARE_MAX": 500000,
+        "colete": {
+          "colet": [
+            {
+              "id_colet": 25,
+              "greutate": 12,
+              "valoare": 3259,
+              "continut_colet": "Televizor Samsung 125cm"
+            },
+            {
+              "id_colet": 26,
+              "greutate": 14,
+              "valoare": 3219,
+              "continut_colet": "Telefon Philips"
+            },
+            {
+              "id_colet": 27,
+              "greutate": 14,
+              "valoare": 2419,
+              "continut_colet": "Televizor Allview"
+            },
+            {
+              "id_colet": 28,
+              "greutate": 10,
+              "valoare": 2019,
+              "continut_colet": "Televizor Philips"
+            },
+            {
+              "id_colet": 29,
+              "greutate": 11,
+              "valoare": 2519,
+              "continut_colet": "Televizor Braun"
+            },
+            {
+              "id_colet": 41,
+              "greutate": 10,
+              "valoare": 4219,
+              "continut_colet": "Televizor 7k"
+            },
+            {
+              "id_colet": 42,
+              "greutate": 10,
+              "valoare": 5219,
+              "continut_colet": "Televizor 8k"
+            },
+            {
+              "id_colet": 30,
+              "greutate": 10,
+              "valoare": 2319,
+              "continut_colet": "Televizor 2k"
+            }
+          ]
+        }
+      },
+      {
+        "ID_CATEGORIE": 89,
+        "DENUMIRE_CATEGORIE": "Telefoane mobile",
+        "GREUTATE_MIN": 0,
+        "GREUTATE_MAX": 5,
+        "VALOARE_MIN": 0,
+        "VALOARE_MAX": 500000,
+        "colete": {
+          "colet": [
+            {
+              "id_colet": 31,
+              "greutate": 0,
+              "valoare": 1159,
+              "continut_colet": "Telefon Iphone"
+            },
+            {
+              "id_colet": 32,
+              "greutate": 0,
+              "valoare": 2139,
+              "continut_colet": "Telefon Huawei"
+            },
+            {
+              "id_colet": 33,
+              "greutate": 1,
+              "valoare": 519,
+              "continut_colet": "Telefon Alcatel"
+            },
+            {
+              "id_colet": 34,
+              "greutate": 1,
+              "valoare": 5819,
+              "continut_colet": "Telefon 4A"
+            },
+            {
+              "id_colet": 35,
+              "greutate": 0,
+              "valoare": 4919,
+              "continut_colet": "Telefon P4"
+            },
+            {
+              "id_colet": 36,
+              "greutate": 0,
+              "valoare": 3419,
+              "continut_colet": "Telefon Iro"
+            },
+            {
+              "id_colet": 37,
+              "greutate": 1,
+              "valoare": 4219,
+              "continut_colet": "Telefon 5B"
+            },
+            {
+              "id_colet": 38,
+              "greutate": 1,
+              "valoare": 12219,
+              "continut_colet": "Telefon 5B"
+            },
+            {
+              "id_colet": 39,
+              "greutate": 0,
+              "valoare": 4219,
+              "continut_colet": "Telefon 5A"
+            },
+            {
+              "id_colet": 40,
+              "greutate": 0,
+              "valoare": 1519,
+              "continut_colet": "Telefon 5A"
+            }
+          ]
+        }
+      }
+    ]
+  }
+}]
